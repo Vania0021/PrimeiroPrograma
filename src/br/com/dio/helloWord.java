623cdd35f849bdd8051349cd36beaf29a6c41278
Elcivânia Nascimento <silvaelci17@gmail.com>
2022-01-05 14:16:57 -0300

    First commit

---
+++ src/br/com/dio/helloWord.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+public class helloWord {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*{ int a = 20;
+		int b = 1;
+		
+		System.out.println("Olá, Java! " + (a+b));
+
+	}*/
+}
+
+}
